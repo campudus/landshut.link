5b39bdc0f00a327c5960d4322172026aa05b3cc5
Konstantin Axt <axtkonstantin@hotmail.de>
2015-01-22 08:54:30 +0100

    tiles prototype

---
+++ prototype/js/app.js
@@ -0,0 +1,36 @@
+// Foundation JavaScript
+// Documentation can be found at: http://foundation.zurb.com/docs
+$(document).foundation();
+
+$(".tiles-content").mCustomScrollbar();
+
+function getElementHeight(elem) {
+  return $(elem).height();
+}
+
+var $footHeight = getElementHeight(".site-footer");
+var $navHeight = getElementHeight(".top-bar");
+var $browserHeight = getElementHeight(window);
+
+function getTilesHeight() {
+  var $tilesHeight = ($browserHeight - ($footHeight + $navHeight)) / 2;
+  return $tilesHeight;
+}
+
+function getContentTilesHeight(){
+  var $tilesHeight = ($browserHeight - ($footHeight + $navHeight))
+  return $tilesHeight;
+}
+
+var $contentTilesHeight = getContentTilesHeight();
+var $tilesHeight = getTilesHeight();
+
+console.log($tilesHeight);
+
+$(".post").css({
+  "height": $tilesHeight
+});
+
+$(".tiles-content").css({
+  "height": $contentTilesHeight
+});
