68ea9fad7ffd6b10c1b1f7989dd3c0fecd09b656
Konstantin Axt <axtkonstantin@hotmail.de>
2015-03-05 09:48:17 +0100

    link site

---
+++ js/app.js
@@ -0,0 +1,12 @@
+// Foundation JavaScript
+// Documentation can be found at: http://foundation.zurb.com/docs
+
+
+$(document).foundation();
+$('body').flowtype({
+  minimum   : 500,
+  maximum   : 1200,
+  minFont   : 12,
+  maxFont   : 40,
+  fontRatio : 30
+});
