e44c9e20baa21c40a6b440433c358f3a7145e14d
axtkonstantin@hotmail.de <e5,oqI,no>
2015-05-29 11:08:26 +0200

    Add lightbox-gallery

--- js/app.js
+++ js/app.js
@@ -4,9 +4,44 @@
 
 $(document).foundation();
 $('body').flowtype({
-  minimum   : 500,
-  maximum   : 1200,
-  minFont   : 12,
-  maxFont   : 40,
+  minimum : 500,
+  maximum : 1200,
+  minFont : 12,
+  maxFont : 40,
   fontRatio : 30
+});
+
+$(document).ready(function () {
+  $('.gallery').magnificPopup({
+    items : [
+      {
+        src : "img/gallery/linkwork-landshut1.jpg"
+      },
+      {
+        src : "img/gallery/linkwork-landshut2.jpg"
+      },
+      {
+        src : "img/gallery/linkwork-landshut3.jpg"
+      },
+      {
+        src : "img/gallery/linkwork-landshut4.jpg"
+      },
+      {
+        src : "img/gallery/linkwork-landshut5.jpg"
+      },
+      {
+        src : "img/gallery/linkwork-landshut6.jpg"
+      },
+      {
+        src : "img/gallery/linkwork-landshut7.jpg"
+      },
+      {
+        src : "img/gallery/linkwork-landshut8.jpg"
+      }
+    ],
+    gallery : {
+      enabled : true
+    },
+    type : 'image'
+  });
 });
